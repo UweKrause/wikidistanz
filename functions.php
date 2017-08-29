d21ccf5dc8427df50a3ffbc62a2d0a0bc64cd9f0
Uwe Krause <uwekrause@gmail.com>
2017-08-29 18:57:00 +0000

    code translation to international

--- functions.php
+++ functions.php
@@ -1,24 +1,22 @@
 <?php
 
 /**
- * Programmkonstanten
+ * Constants for the program
  */
  
- // hier ggf. Benutzereingabe uebernehmen
- define('LAENDERCODE', 'de');
  
- define('CACHEDIR', 'cache_'. LAENDERCODE. '_article');
+ define('CACHEDIR', 'cache_'. AREACODE. '_article');
  
- define('BACKLINKCACHEDIR', 'cache_'. LAENDERCODE. '_backlinks/');
- define('ARTICLECACHEDIR', 'cache_'. LAENDERCODE. '_article/');
+ define('BACKLINKCACHEDIR', 'cache_'. AREACODE. '_backlinks/');
+ define('ARTICLECACHEDIR', 'cache_'. AEREACODE. '_article/');
 
 
 
 /**
- * Konstanten fuer den API Aufruf
+ * Constants for API call
  */
  
-define('API_Base', 'https://'. LAENDERCODE.'.wikipedia.org/w/api.php');
+define('API_Base', 'https://'. AREACODE.'.wikipedia.org/w/api.php');
 define('API_Action', '?action=query&utf8=true&format=json');
 
 define('API_Backlinks_Artikel', '&list=backlinks&blnamespace=0&bllimit=max');
@@ -26,9 +24,10 @@ define('API_Backlinks_Artikel', '&list=backlinks&blnamespace=0&bllimit=max');
 define('API_Prop_Artikellinks', '&prop=links&plnamespace=0&pllimit=max');
 
 /**
- * Unterfunktionen
+ * Prepare and load subfunctions
  */
 
+require_once('functions/server_util.php');
 require_once('functions/cache_links.php');
 require_once('functions/cache_backlinks.php');
 require_once('functions/microtime_float.php');
